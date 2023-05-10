b325b7795967bc9774e09d4171775a7da54f6a6a
mosaj <51008278+m2utester3@users.noreply.github.com>
2023-05-11 00:06:33 +0800

    TS5

---
+++ Test Suites/TS_MAE/MAE_TS5.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MAE_TS5</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7d63be82-ec0f-44ac-a088-40de6f882a63</testSuiteGuid>
+   <testCaseLink>
+      <guid>0276519e-8c1e-43b2-83ff-86fe449b112c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile TC/MAE UAT Onboarding new app</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
