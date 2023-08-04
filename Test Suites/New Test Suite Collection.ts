fc0f42a5ed423b3d3a9e14579d5ba110b6955ce4
mosaj <51008278+m2utester3@users.noreply.github.com>
2023-08-04 12:23:51 +0800

    multi profile

--- Test Suites/New Test Suite Collection.ts
+++ Test Suites/New Test Suite Collection.ts
@@ -20,7 +20,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
+            <profileName>default - Copy</profileName>
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
@@ -30,7 +30,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
+            <profileName>default - Copy</profileName>
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
